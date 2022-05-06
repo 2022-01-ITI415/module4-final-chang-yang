ed541fb096dedd85669846ca3f3cebb0aa67a8b7
Xiaotian Yang <1977557517@qq.com>
2022-05-05 23:46:57 -0400

    Add 2 AutoDoor

---
+++ Assets/Script/AutoDoor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoor : MonoBehaviour
+{
+    private Animator mAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            mAnimator.SetTrigger("TrOpen");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            mAnimator.SetTrigger("TrClose");
+        }
+    }
+}
